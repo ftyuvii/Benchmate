b3eeb1cf150bee85b180c36e53eca019b807518a
Yuvraj Ab <yuvraajapte@gmail.com>
2026-09-22 17:50:20 +0530

    Remove base path from Vite configuration
    Removed base path configuration for GitHub Pages.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Benchmate/', // <-- Ye line zaroori hai GitHub Pages ke liye!
 })
